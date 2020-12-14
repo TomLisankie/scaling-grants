543a6e80b9085da98beee6162f7fd84f9b6a295c
Thomas <thomas.lisankie@gmail.com>
2020-12-14 12:59:40 -0500

    Created functions for getting info about a competition

--- lfc_test.py
+++ lfc_test.py
@@ -4,14 +4,23 @@ import mwclient
 
 load_dotenv()
 
-site = mwclient.Site('torque.leverforchange.org/', '100Change2020/', scheme="https")
-site.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
-proposalData = site.api('torquedataconnect', format='json', path='/LFC100Change2020')
-# 100
-print(len(proposalData["LFC100Change2020"]))
+def mediawiki_formatted_competition_name(competition_name):
+    if competition_name[0].isdigit():
+        return "LFC" + competition_name
+    return competition_name
+
+def get_proposals_for_competition(competition_name):
+    site = mwclient.Site('torque.leverforchange.org/', competition_name + '/', scheme="https")
+    site.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+    proposals = site.api('torquedataconnect', format='json', path='/' + mediawiki_formatted_competition_name(competition_name))
+    return proposals
+
+proposal_data = get_proposals_for_competition("100Change2020")
+# 455
+print(len(proposal_data["LFC100Change2020"]))
 print()
 # Empower 25 million people with safe water and sanitation
-print(next(filter(lambda p: p["Review Number"] == "6988", proposalData["LFC100Change2020"]))['Project Title'])
+print(next(filter(lambda p: p["Review Number"] == "6988", proposal_data["LFC100Change2020"]))['Project Title'])
 print()
 # All available columns
-print(proposalData["LFC100Change2020"][0].keys())
+print(proposal_data["LFC100Change2020"][0].keys())
