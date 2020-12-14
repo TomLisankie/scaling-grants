c8100bb6415ba1eab1e2f9fd3687232192aa0dd7
Thomas <thomas.lisankie@gmail.com>
2020-12-14 13:05:32 -0500

    Made use of the functions created in the last commit in more places and added a comment on why competition names need to be formatted for MediaWiki

--- lfc_test.py
+++ lfc_test.py
@@ -5,6 +5,7 @@ import mwclient
 load_dotenv()
 
 def mediawiki_formatted_competition_name(competition_name):
+    # The reason this function exists is because MediaWiki has some weird thing with entities not starting with numbers
     if competition_name[0].isdigit():
         return "LFC" + competition_name
     return competition_name
@@ -15,12 +16,14 @@ def get_proposals_for_competition(competition_name):
     proposals = site.api('torquedataconnect', format='json', path='/' + mediawiki_formatted_competition_name(competition_name))
     return proposals
 
-proposal_data = get_proposals_for_competition("100Change2020")
+competition_name = "100Change2020"
+mw_formatted_name = mediawiki_formatted_competition_name(competition_name)
+proposal_data = get_proposals_for_competition(competition_name)
 # 455
-print(len(proposal_data["LFC100Change2020"]))
+print(len(proposal_data[mw_formatted_name]))
 print()
 # Empower 25 million people with safe water and sanitation
-print(next(filter(lambda p: p["Review Number"] == "6988", proposal_data["LFC100Change2020"]))['Project Title'])
+print(next(filter(lambda p: p["Review Number"] == "6988", proposal_data[mw_formatted_name]))['Project Title'])
 print()
 # All available columns
-print(proposal_data["LFC100Change2020"][0].keys())
+print(proposal_data[mw_formatted_name][0].keys())
